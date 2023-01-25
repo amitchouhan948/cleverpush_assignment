e52690a1bf22c7ac588a3aedf9c3518236b27fe3
Amit Singh Chouhan <amitchouhan948@gmail.com>
2023-01-25 13:14:06 +0530

    server done

--- Backend/src/Routes/Search.route.js
+++ Backend/src/Routes/Search.route.js
@@ -4,10 +4,34 @@ const app = express.Router();
 
 app.get("/", async (req, res) => {
 
+    const query=(req.query.q)
+
+
+    if(query){
+
+        console.log(query)
+        try{
+
+            let data= await axios.get(`https://pixabay.com/api/?key=33103512-d0b611af2ceddf95d67d63d03&q=${query}`)
+            console.log("x")
+           return res.send(data.data);
+    
+        }
+        catch(err){
+        
+            return res.send(err)
+    
+        }
+
+
+
+
+    }
+
     try{
 
         let data= await axios.get("https://pixabay.com/api/?key=33103512-d0b611af2ceddf95d67d63d03&q=man")
-
+            console.log("y")
         res.send(data.data);
 
     }
