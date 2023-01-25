7eb356746249aa1d9a59b1bbabe80cc6fdf6a124
Amit Singh Chouhan <amitchouhan948@gmail.com>
2023-01-25 13:13:48 +0530

    Frontend

--- Frontend/cleverpush-assign-frontend/src/App.js
+++ Frontend/cleverpush-assign-frontend/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
  <>
-  <Navbar/>
+  {/* <Navbar/> */}
   <Homepage />
  </>
   );
--- Frontend/cleverpush-assign-frontend/src/components/Homepage/Homepage.js
+++ Frontend/cleverpush-assign-frontend/src/components/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import { Card, SimpleGrid } from '@chakra-ui/react'
+import { Button, Card, Flex, Image, Input, SimpleGrid } from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Imagecard from './Imagecard'
@@ -7,6 +7,10 @@ import Imagecard from './Imagecard'
 const Homepage = () => {
 
     const [data,setData]=useState(false)
+
+    const [query,setQuery]=useState("") 
+
+    
 
     useEffect(()=>{
 
@@ -17,19 +21,39 @@ const Homepage = () => {
 
     const getData= async()=>{
 
-        let res=await axios.get("http://localhost:8000/search")
+        let res=await axios.get("https://pixabay-backend.onrender.com/search")
     
-        console.log(res.data.hits)
-        setData(res.data.hits)
+        let y=[...res.data.hits]
+        setData(y)
 
     
     
      }
 
+     const getSearchResult=async()=>{
+
+        let res=await axios.get(`https://pixabay-backend.onrender.com/search?q=${query}`)
+        console.log(query)
+        console.log(res.data.hits)
+        setData([...res.data.hits])
+     }
+
 
   return (
     <>
-        <SimpleGrid>
+      <Flex alignItems="center" justifyContent="space-between" width="100%" m={'auto'} pt="10px" pb="5px" bgColor={"black"} pl="20px" pr="20px">
+
+        <Image width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Pixabay-logo-new.svg/300px-Pixabay-logo-new.svg.png" alt="logo" />
+
+        <Flex>
+            <Input onChange={(e)=>setQuery(e.target.value)} mr="20px" bgColor={"white"} placeholder="Search Anything" />
+            <Button onClick={()=>getSearchResult()}>Search</Button>
+        </Flex>
+
+
+
+    </Flex>
+        <SimpleGrid columns={4} spacing={10} m="auto" width="95%" mt="20px">
 
              {data &&   data.map((el)=><Imagecard pic={el.webformatURL}/>)}
 
