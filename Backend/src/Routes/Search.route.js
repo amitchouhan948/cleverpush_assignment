5ccb927205aa7e50336b4c7784c5f0d5372c49ac
Amit Singh Chouhan <amitchouhan948@gmail.com>
2023-01-29 22:35:54 +0530

    checking data

--- Backend/src/Routes/Search.route.js
+++ Backend/src/Routes/Search.route.js
@@ -31,7 +31,7 @@ app.get("/", async (req, res) => {
     try{
 
         let data= await axios.get("https://pixabay.com/api/?key=33103512-d0b611af2ceddf95d67d63d03&q=man")
-            console.log("y")
+            console.log("y", data)
         res.send(data.data);
 
     }
