9ac4e0042a0c56df0c18837553d522475e782e2b
Amit Singh Chouhan <amitchouhan948@gmail.com>
2023-01-30 17:12:19 +0530

    modal component

--- Frontend/cleverpush-assign-frontend/src/components/Modal/Modal.js
+++ Frontend/cleverpush-assign-frontend/src/components/Modal/Modal.js
@@ -1,8 +1,12 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
+   <>
+   <Box>
+    <Text>Modal to be added here</Text>
+    </Box></>
   )
 }
 
